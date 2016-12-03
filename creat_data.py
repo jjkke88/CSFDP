53e95488b570c44fd5723ca10406d6b202bc3c4a
jjkke88 <wuyupei@wegoeur.com>
2016-12-03 20:51:52 +0800

    good classfify

--- creat_data.py
+++ creat_data.py
@@ -14,23 +14,26 @@ if __name__ == "__main__":
         return points
     points_number = 300
     points_range = np.array((0, 100))
-    x = get_random_number(points_number/2, points_range/2)
-    y = get_random_number(points_number/2, points_range/2)
-    x1 = get_random_number(points_number / 2 , points_range / 2+50)
-    y1 = get_random_number(points_number / 2 , points_range / 2+50)
+    x_all = []
+    y_all =[]
+    for i in range(3):
+        x = get_random_number(points_number/2, (i*30, i*30+30))
+        y = get_random_number(points_number/2, (i*30, i*30+30))
+        x_all.append(x)
+        y_all.append(y)
 
-    x = np.append(x, x1)
-    y = np.append(y, y1)
-    plt.plot(x , y , 'rs')
-    # plt.show()
+    x_all=np.concatenate(np.array(x_all))
+    y_all = np.concatenate(np.array(y_all))
+    plt.plot(x_all , y_all , 'rs')
+    plt.show()
     fp = open("raw_data.txt" , 'wb')
-    for i in range(points_number):
-        fp.write(str(x[i])+" "+str(y[i])+'\n')
+    for i in range(x_all.shape[0]):
+        fp.write(str(x_all[i])+" "+str(y_all[i])+'\n')
     fp.close()
     fp = open("dist_data.dat", "wb")
-    for i in range(points_number):
-        for j in range(points_number):
-            dist = np.sqrt(np.square(x[i]-x[j]) + np.square(y[i]-y[j]))
+    for i in range(x_all.shape[0]):
+        for j in range(x_all.shape[0]):
+            dist = np.sqrt(np.square(x_all[i]-x_all[j]) + np.square(y_all[i]-y_all[j]))
             fp.write(str(i+1)+'\t'+str(j+1)+'\t'+str(dist)+'\n')
     fp.close()
 
--- data_process.py
+++ data_process.py
@@ -3,6 +3,7 @@ matplotlib.use("Qt4Agg")
 import numpy as np
 from util import *
 import matplotlib.pyplot as plt
+import parameters as pms
 
 """process data"""
 class DataProcess(object):
@@ -14,6 +15,7 @@ class DataProcess(object):
         self.sigma_min = sigma_min
         self.get_data_from_file()
         self.calculate_sigma()
+        self.plt_show()
         self.get_center()
         self.classify_others()
 
@@ -27,15 +29,28 @@ class DataProcess(object):
         raw_data = np.genfromtxt(self.file_name , delimiter=self.delimiter , names=['x', 'y', 'distance'], dtype="i8,i8,f8")
         self.sample_number = raw_data[-1][1] # number of sample points
         distance_matrix = np.zeros((self.sample_number, self.sample_number))
+
         rho_matrix = np.zeros((self.sample_number, self.sample_number))
         for index in raw_data:
             distance_matrix[index[0] - 1][index[1] - 1] = index[2]
             distance_matrix[index[1] - 1][index[0] - 1] = index[2]
+        # distance_vector = np.reshape(distance_matrix, (self.sample_number * self.sample_number))
+        # distance_vector_nonzero = []
+        # for i in distance_vector:
+        #     if i!=0:
+        #         distance_vector_nonzero.append(i)
+        # distance_vector_sort_nonzero = np.sort(np.array(distance_vector_nonzero))
+        # self.d_c = distance_vector_nonzero[self.sample_number * pms.d_c_ratio]
+        average_distance = np.mean(distance_matrix)
+        self.d_c = average_distance * pms.d_c_ratio
+        print "d_c:"+str(self.d_c)
+        for index in raw_data:
             rho_matrix[index[0] - 1][index[1] - 1] += kafang_distribution(index[2] - self.d_c)
             rho_matrix[index[1] - 1][index[0] - 1] += kafang_distribution(index[2] - self.d_c)
         self.distance_matrix = distance_matrix
         self.rho_matrix = rho_matrix
         self.rho_vector = np.sum(self.rho_matrix, axis=1)
+        print "average_neighbour_number:"+str(np.mean(self.rho_vector))
 
     def calculate_sigma(self):
         sigma_vector = np.zeros(self.sample_number)
@@ -44,6 +59,9 @@ class DataProcess(object):
             if fit_index_array.shape[0]>0:
                 fit_distance_vector = self.distance_matrix[i][fit_index_array]
                 sigma_vector[i] = np.min(fit_distance_vector)
+            else:
+                # for the sample point that has largest rho
+                sigma_vector[i] = np.max(self.distance_matrix[i])
         self.sigma_vector = sigma_vector
 
     def get_center(self):
@@ -63,10 +81,12 @@ class DataProcess(object):
             result[self.center[center_no]] = center_no
         rho_sort_desc_index = np.argsort(-self.rho_vector)  # sort sample points descent by rho
         for i in range(self.sample_number):
-            index_array = rho_sort_desc_index[:i]
-            nearest_point_index = self.get_cluster_result(rho_sort_desc_index[i], index_array)
-            result[i] = result[nearest_point_index]
-        self.result = result
+            now_point_index = rho_sort_desc_index[i] # the point now is going to be classified
+            if now_point_index not in self.center:
+                index_array = rho_sort_desc_index[:i]
+                nearest_point_index = self.get_cluster_result(now_point_index, index_array)
+                result[now_point_index] = result[nearest_point_index]
+            self.result = result
 
     def calculate_distance(self, index, index_array):
         """
@@ -75,6 +95,8 @@ class DataProcess(object):
         :param index_array:
         :return:
         """
+        if index_array.shape[0]==0:
+            raise NameError("index_array can not be empty")
         return self.distance_matrix[index][index_array]
 
     def get_cluster_result(self, index, index_array):
@@ -87,12 +109,49 @@ class DataProcess(object):
         nearest_index = np.argmin(distance)
         return index_array[nearest_index]
 
+    def plt_show(self):
+        plt.plot(self.rho_vector , self.sigma_vector , 'rs')
+        plt.show()
+
 if __name__ == "__main__":
-    data_process = DataProcess("dist_data.dat", '\t', 50, 16, 0.13)
+    def draw_different_point(raw_data, cluster_result, cls=0):
+        plt.figure()
+        x = []
+        y = []
+        color = []
+        for raw in range(len(raw_data)):
+            if cls is None:
+                x.append(raw_data[raw][0])
+                y.append(raw_data[raw][1])
+                color.append(np.cos(cluster_result[raw] * 5))
+            elif cluster_result[raw]==cls:
+                x.append(raw_data[raw][0])
+                y.append(raw_data[raw][1])
+                color.append(np.cos(cluster_result[raw]*5))
+        plt.scatter(x , y , c=color , s=25 , marker='o')
+
+    def draw_center_point(raw_data, center, cluster_result):
+        plt.figure()
+        x = [raw_data[i][0] for i in center]
+        y = [raw_data[i][1] for i in center]
+        color = [np.cos(cluster_result[i]) for i in center]
+        plt.scatter(x , y , c=color , s=25 , marker='o')
+
+
+
+    """
+    def __init__(self, file_name, delimiter, d_c, rho_min, sigma_min):
+    """
+    data_process = DataProcess("dist_data.dat", '\t', 0, 90, 20)
     # print data_process.rho_vector
     # print data_process.rho_vector
     # print
     # print data_process.sigma_vector
-    plt.plot(data_process.rho_vector, data_process.sigma_vector, 'rs')
-    plt.show()
+
     print data_process.result
+    raw_data = np.genfromtxt("raw_data.txt", delimiter=" ", names=['x' , 'y'] ,
+                             dtype="f8,f8")
+
+    draw_different_point(raw_data, data_process.result, cls=None)
+    draw_center_point(raw_data, data_process.center, data_process.result)
+    plt.show()
